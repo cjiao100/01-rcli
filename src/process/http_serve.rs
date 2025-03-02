22f33217e6a50994c9acbe9947bd9b514458a7f7
cjiao100 <1971416012@qq.com>
2025-03-02 19:40:47 +0800

    feat: 更新 HTTP 服务，添加 tower-http 支持，优化文件服务功能

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -1,12 +1,13 @@
 use anyhow::Result;
 use axum::{
     extract::{Path, State},
-    http::StatusCode,
+    http::{Request, StatusCode},
     routing::get,
     serve, Router,
 };
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
 use tokio::{fs, net::TcpListener};
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
 
 #[derive(Debug)]
@@ -20,13 +21,24 @@ pub async fn process_http_serve(dir: PathBuf, port: u16) -> Result<()> {
     info!("Serving {:?} on port {}", dir, addr);
 
     // state 会被传递给每个请求处理函数
-    let state = HttpServeState { dir };
+    let state = HttpServeState { dir: dir.clone() };
+
+    let dir_service = ServeDir::new(dir)
+        .append_index_html_on_directories(true)
+        .precompressed_gzip()
+        .precompressed_br()
+        .precompressed_deflate()
+        .precompressed_zstd();
 
     // 创建一个路由器 router 并添加路由
     // 路由器 router 会将请求映射到处理函数
     // 通过 with_state 方法将 state 传递给路由器
     // 这样在处理函数中就可以访问 state 了
     let router = Router::new()
+        // route_service 是一个中间件，用于将请求映射到服务
+        // .route_service("/tower", dir_service)
+        // nest_service 是一个中间件，用于将请求映射到服务, 并添加前缀, 这里添加了前缀 /tower, 所以请求路径是 /tower/*, 会映射到 dir_service
+        .nest_service("/tower", dir_service)
         // 这里使用了一个通配符 {*path}，可以匹配任意路径
         // 通过 Path 提取器可以提取请求路径中的参数
         // 通过 State 提取器可以提取 state
@@ -35,7 +47,12 @@ pub async fn process_http_serve(dir: PathBuf, port: u16) -> Result<()> {
         // 通过 Arc 包装 state，可以在多个请求中共享 state
         // state 最好可以使用 Arc 进行封装一下，不然每次请求都会克隆一份，如果 state 比较大，会影响性能
         // Arc 封装后，只会增加引用计数，不会克隆数据，所以性能会更好
-        .with_state(Arc::new(state));
+        .with_state(Arc::new(state))
+        .layer(tower_http::trace::TraceLayer::new_for_http().on_request(
+            |req: &Request<_>, _: &_| {
+                tracing::info!("Request: {} {}", req.method(), req.uri());
+            },
+        ));
 
     // 创建一个 TcpListener 并绑定到指定地址
     let listener = TcpListener::bind(addr).await?;
@@ -99,5 +116,22 @@ pub async fn file_handler(
             warn!("Read file error: {:?}", e);
             (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            dir: PathBuf::from("."),
+        });
+
+        let path = Path("Cargo.toml".to_string());
+        let (status, content) = file_handler(State(state), path).await;
+
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.trim().starts_with("[package]"));
     }
 }
